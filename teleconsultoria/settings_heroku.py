607343b82b9cfa25bd47ef0d3b1f56cfb6ad487d
Lucas Cruz <lucasdasilvacruz@gmail.com>
2021-01-03 18:58:35 -0300

    Update settings_heroku.py

--- teleconsultoria/settings_heroku.py
+++ teleconsultoria/settings_heroku.py
@@ -8,3 +8,15 @@ ALLOWED_HOSTS = ['telessaude-ac.herokuapp.com',]
 db_from_env = dj_database_url.config()
 
 DATABASES['default'].update(db_from_env)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'teleconsultoria',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
